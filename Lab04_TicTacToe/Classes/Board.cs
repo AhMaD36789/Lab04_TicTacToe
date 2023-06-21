ed324e1b834613581bc22723a70f3291ccf9012b
Ahmad Harhsheh <45499233+AhMaD36789@users.noreply.github.com>
2023-06-21 20:43:13 +0300

    returning board

--- Lab04_TicTacToe/Classes/Board.cs
+++ Lab04_TicTacToe/Classes/Board.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab04_TicTacToe.Classes
+{
+    public class Board
+    {
+        /// <summary>
+        /// Tic Tac Toe Gameboard states
+        /// </summary>
+        public string[,] GameBoard = new string[,]
+        {
+            {"1", "2", "3"},
+            {"4", "5", "6"},
+            {"7", "8", "9"},
+        };
+
+
+        public void DisplayBoard()
+        {
+            //prints game board used in game class
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"{GameBoard[i, j]}\t");
+                }
+                Console.WriteLine("\n");
+                Console.WriteLine("--------------------");
+            }
+
+        }
+    }
+}
